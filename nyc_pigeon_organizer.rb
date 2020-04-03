57612f8a8adf8bf81ac600e217860e8c95bce16b
Gregory Richens <gregoryrichens@gmail.com>
2020-04-03 21:07:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,8 +13,10 @@ def nyc_pigeon_organizer(pigeon_data)
   end
   pigeon_list.each do |(name, attribute_hash)|
     attribute_hash.each do |(attribute, value_array)|
-      pigeon_data.each do |(attribute, value_hash)|
+      pigeon_data.each do |(attribute2, value_hash)|
         value_hash.each do |value, name_array|
-          name_array.each do |name|
-            if name == 
+          name_array.each do |name2|
+            if name2 == name &&  attribute2 == attribute
+              name_array << value.to_s
+              
 end
